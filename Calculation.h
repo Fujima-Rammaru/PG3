fd56b4425655751868095ddb90e7ecb3fad6e402
藤間嵐丸 <146901376+Fujima-Rammaru@users.noreply.github.com>
2024-05-24 16:16:22 +0900

    04_02確認課題完了

---
+++ Calculation.h
@@ -0,0 +1,25 @@
+#pragma once
+#include"stdio.h"
+template <typename Type1, typename Type2>
+
+class Calculation {
+
+public:
+
+	//メンバ変数
+	Type1 a;
+	Type2 b;
+
+	//コンストラクタ
+	Calculation(Type1 a, Type2 b) :a(a), b(b) {};
+
+	//2つの引数の値を比べて小さいほうの値を返す
+	Type1 Min() {
+		if (a <= b) {//戻り値は引数によって変化するのでキャストを使って整える
+			return	static_cast<Type1> (a);
+		}
+		else if (b < a) {
+			return static_cast<Type1> (b);
+		}
+	}
+};
--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include<stdio.h>
-#include"Enemy.h"
+#include"Calculation.h" 
 
 
 int main() {
-	Enemy* enemy;
+	Calculation<int, int>       num1(1, 2);
+	Calculation<float, float>   num2(3.1f, 2.9f);
+	Calculation<double, double> num3(4.5, 4.56);
+	Calculation<int, float>     num4(4, 4.5f);
+	Calculation<int, double>    num5(4, 3.99);
+	Calculation<double, float>  num6(4.21, 4.5f);
 
-	enemy = new Enemy;
-
-	enemy->Update();
-
-	delete enemy;
+	printf("%d\n", num1.Min());
+	printf("%3.2f\n", num2.Min());
+	printf("%lf\n", num3.Min());
+	printf("%d\n", num4.Min());
+	printf("%d\n", num5.Min());
+	printf("%lf\n", num6.Min());
 
 	return 0;
 }
